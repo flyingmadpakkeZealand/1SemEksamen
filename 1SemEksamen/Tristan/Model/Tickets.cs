7478429eeefd2c84c9233c593a1f0c6c2bc1b3ba
Tristan Taylor Kamp <tris0032@edu.easj.dk>
2019-12-03 15:16:29 +0100

    Dunno whats changed

---
+++ 1SemEksamen/Tristan/Model/Tickets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1SemEksamen.Tristan.Model
+{
+    class Tickets
+    {
+        public int Barn { get; set; }
+        public int Voksen { get; set; }
+        public int Pensionist { get; set; }
+        public int KortEllerStudent { get; set; }
+
+
+    }
+}
