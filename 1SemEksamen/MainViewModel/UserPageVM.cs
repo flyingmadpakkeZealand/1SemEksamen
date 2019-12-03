cb4854c8180d203f306840ee5f3a7ae44b1a5545
Magnus Bahn Berger <magn351i@edu.easj.dk>
2019-12-03 15:14:48 +0100

    Added a menu to UserPage. Added a MainVM folder for viewmodels that are more general purpose.

---
+++ 1SemEksamen/MainViewModel/UserPageVM.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using _1SemEksamen.Annotations;
+using _1SemEksamen.Common;
+
+namespace _1SemEksamen.MainViewModel
+{
+    public class UserPageVM : INotifyPropertyChanged
+    {
+        private bool _menuVisibility;
+
+        public bool MenuVisibility
+        {
+            get { return _menuVisibility; }
+            set
+            {
+                _menuVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public UserPageVM()
+        {
+            _menuVisibility = false;
+            _pressToggleMenuCommand = new RelayCommand(ToggleMenu);
+        }
+
+        private RelayCommand _pressToggleMenuCommand;
+
+        public ICommand PressToggleMenuCommand
+        {
+            get { return _pressToggleMenuCommand; }
+        }
+
+
+        public void ToggleMenu()
+        {
+            MenuVisibility = !MenuVisibility;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
