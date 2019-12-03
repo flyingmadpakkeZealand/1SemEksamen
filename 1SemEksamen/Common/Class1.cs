6f568c374b7bc56dc6febb1e7631881329e5e8f1
Magnus Bahn Berger <magn351i@edu.easj.dk>
2019-12-03 13:49:41 +0100

    Initial setup.

---
+++ 1SemEksamen/Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1SemEksamen.Common
+{
+    class Class1
+    {
+    }
+}
