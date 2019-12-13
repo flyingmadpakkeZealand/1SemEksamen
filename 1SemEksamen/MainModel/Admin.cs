9e074f77e2fcad00465fa0962582825dbdca5e23
Magnus Bahn Berger <magn351i@edu.easj.dk>
2019-12-13 11:04:16 +0100

    Updated MainPage with a style and some login functionality (WIP). Added FileNotSavedException and made changes to PersistencyFacade such that it throws this exception when a save file couldn't be located. Added conceptual classes User and Admin. Added an empty AdminPage. Changed MainPageVM to allow users to login. Added unit testing.

---
+++ 1SemEksamen/MainModel/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1SemEksamen.MainModel
+{
+    //Admin inherits from user just in case Admin gets more specialized.
+    public class Admin:User
+    {
+        public Admin(string userName, string password) : base(true, userName, password)
+        {
+        }
+    }
+}
---
+++ UnitTest_1SemEksamen/UnitTestTemplate.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_1SemEksamen
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
